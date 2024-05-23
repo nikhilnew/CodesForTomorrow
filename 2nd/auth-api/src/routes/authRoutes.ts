9cce4e158f8a737041e2b1eb12d6d2faff0dc851
Nikhil kurmi <89118026+nikhilnew@users.noreply.github.com>
2024-05-23 21:57:38 +0530

    Update authRoutes.ts

--- 2nd/auth-api/src/routes/authRoutes.ts
+++ 2nd/auth-api/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getUserDetails, forgotPassword, resetPassword } from '../controllers/authControllers';
+import { signup, login, getUserDetails, forgotPassword, resetPassword } from '../controllers/authControllers';
 import { authenticate } from '../middlewares/authMiddleware';
 
 const router = Router();
 
+router.post('/signup', signup);
+router.post('/login', login);
 router.get('/user', authenticate, getUserDetails);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
